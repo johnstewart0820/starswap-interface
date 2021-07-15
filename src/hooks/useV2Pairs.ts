f837757037c1a91af5995ffc176e4090c66e8fae
Zehua Ren <renzholy@gmail.com>
2021-07-15 21:23:22 +0800

    avoid crash

--- src/hooks/useV2Pairs.ts
+++ src/hooks/useV2Pairs.ts
@@ -21,42 +21,43 @@ export function useV2Pairs(currencies: [Currency | undefined, Currency | undefin
     [currencies]
   )
 
-  const pairAddresses = useMemo(
-    () =>
-      tokens.map(([tokenA, tokenB]) => {
-        return tokenA &&
-          tokenB &&
-          tokenA.chainId === tokenB.chainId &&
-          !tokenA.equals(tokenB) &&
-          V2_FACTORY_ADDRESSES[tokenA.chainId]
-          ? computePairAddress({ factoryAddress: V2_FACTORY_ADDRESSES[tokenA.chainId], tokenA, tokenB })
-          : undefined
-      }),
-    [tokens]
-  )
+  // const pairAddresses = useMemo(
+  //   () =>
+  //     tokens.map(([tokenA, tokenB]) => {
+  //       return tokenA &&
+  //         tokenB &&
+  //         tokenA.chainId === tokenB.chainId &&
+  //         !tokenA.equals(tokenB) &&
+  //         V2_FACTORY_ADDRESSES[tokenA.chainId]
+  //         ? computePairAddress({ factoryAddress: V2_FACTORY_ADDRESSES[tokenA.chainId], tokenA, tokenB })
+  //         : undefined
+  //     }),
+  //   [tokens]
+  // )
 
-  const results = useMultipleContractSingleData(pairAddresses, PAIR_INTERFACE, 'getReserves')
+  // const results = useMultipleContractSingleData(pairAddresses, PAIR_INTERFACE, 'getReserves')
 
-  return useMemo(() => {
-    return results.map((result, i) => {
-      const { result: reserves, loading } = result
-      const tokenA = tokens[i][0]
-      const tokenB = tokens[i][1]
+  // return useMemo(() => {
+  //   return results.map((result, i) => {
+  //     const { result: reserves, loading } = result
+  //     const tokenA = tokens[i][0]
+  //     const tokenB = tokens[i][1]
 
-      if (loading) return [PairState.LOADING, null]
-      if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
-      if (!reserves) return [PairState.NOT_EXISTS, null]
-      const { reserve0, reserve1 } = reserves
-      const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
-      return [
-        PairState.EXISTS,
-        new Pair(
-          CurrencyAmount.fromRawAmount(token0, reserve0.toString()),
-          CurrencyAmount.fromRawAmount(token1, reserve1.toString())
-        ),
-      ]
-    })
-  }, [results, tokens])
+  //     if (loading) return [PairState.LOADING, null]
+  //     if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
+  //     if (!reserves) return [PairState.NOT_EXISTS, null]
+  //     const { reserve0, reserve1 } = reserves
+  //     const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
+  //     return [
+  //       PairState.EXISTS,
+  //       new Pair(
+  //         CurrencyAmount.fromRawAmount(token0, reserve0.toString()),
+  //         CurrencyAmount.fromRawAmount(token1, reserve1.toString())
+  //       ),
+  //     ]
+  //   })
+  // }, [results, tokens])
+  return useMemo(() => [[PairState.NOT_EXISTS, null]], [])
 }
 
 export function useV2Pair(tokenA?: Currency, tokenB?: Currency): [PairState, Pair | null] {

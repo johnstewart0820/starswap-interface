ea3a4b77298bf25a6e755af8029c020de89b2c0d
Bingo Du <tarvos21@gmail.com>
2021-07-19 14:06:39 +0800

    Update STC logo and border radius

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -1,7 +1,8 @@
 import { Currency } from '@uniswap/sdk-core'
 import React, { useMemo } from 'react'
 import styled from 'styled-components/macro'
-import EthereumLogo from '../../assets/images/ethereum-logo.png'
+// import EthereumLogo from '../../assets/images/ethereum-logo.png'
+import STCLogo from '../../assets/svg/stc.svg'
 import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/wrappedTokenInfo'
 import Logo from '../Logo'
@@ -13,7 +14,7 @@ const StyledEthereumLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
-  border-radius: 24px;
+  border-radius: 4px;
 `
 
 const StyledLogo = styled(Logo)<{ size: string }>`
@@ -58,5 +59,5 @@ export default function CurrencyLogo({
 
   return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} {...rest} />
   */
-  return <StyledEthereumLogo src={EthereumLogo} size={size} style={style} {...rest} />
+  return <StyledEthereumLogo src={STCLogo} size={size} style={style} {...rest} />
 }
